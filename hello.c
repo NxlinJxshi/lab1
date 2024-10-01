1462ec18c52dfacc963bb0402af41aa2d4000c50
NxlinJxshi <103763994+NxlinJxshi@users.noreply.github.com>
2024-10-01 18:27:44 +0000

    string change in hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  char hello[] = "Hello!"; 
-  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello[] = "Halo!"; 
+  char hello2[] = { 72, 64, 108, 111, 33, 0 };
 
   puts(hello);
   puts(hello2);
